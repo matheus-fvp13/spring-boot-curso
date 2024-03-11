d140a4c230d49a40e902744f1ab42a47c315cb87
Matheus Vieira <matheus.fvp@outlook.com>
2024-03-11 14:14:52 -0300

    project created

---
+++ src/main/java/edu/mfvp/springbootcurso/SpringbootCursoApplication.java
@@ -0,0 +1,13 @@
+package edu.mfvp.springbootcurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/mfvp/springbootcurso/SpringbootCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mfvp.springbootcurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
